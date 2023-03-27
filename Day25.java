a34e80a4a70c22b7fa87e7843ea466478b9975a3
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2023-03-27 19:34:17 +0530

    Add files via upload

---
+++ Day25.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class Day25 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the radius of the Circle");
+        double n=sc.nextInt();
+        double aoc=3.142*n*n;
+    System.out.println("The area of the circle is "+aoc);
+    }
+}
