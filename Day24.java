4611f16215a873726c9c0aba72c910f988e3a7df
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2023-03-27 19:33:41 +0530

    Update Day24.java

--- Day24.java
+++ Day24.java
@@ -18,3 +18,28 @@ public class Day24 {
     }
     
 }
+
+/*
+ // Write a java program to print Pyramid of difference of 2 pattern using stars
+
+import java.util.*;
+public class Day24 {
+    public static void main (String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println(" Enter the number of rows");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>i;j--){
+                System.out.print(" ");
+
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
+
+ */
