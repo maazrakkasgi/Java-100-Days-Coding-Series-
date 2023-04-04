823b28785cf438ee3bbd9fc05479a27b29825ad7
maazrakkasgi <115449204+maazrakkasgi@users.noreply.github.com>
2023-04-04 13:02:19 +0530

    Update Day30.java

--- Day30.java
+++ Day30.java
@@ -5,12 +5,11 @@ public class Day30{
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the  string");
     String str1=sc.nextLine();
+    char ch[]=str1.toCharArray();
     int count=0;
-    for(int i=str1.length()-1;i>=0;i--)
-{ count++;
-
-
+    for (char c:ch){
+        count++;
     }
-System.out.println(count);
+    System.out.println(count);
 }
 }
